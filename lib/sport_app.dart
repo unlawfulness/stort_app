13bf7df2d3ea29fe63420988f9ac6e4ad49612bc
unlawfulness <ilmikhaylin@gmail.com>
2024-01-29 13:29:02 +0300

    Насаживаем потихоньку

--- lib/sport_app.dart
+++ lib/sport_app.dart
@@ -2,3 +2,16 @@ import 'package:flutter/material.dart';
 import 'package:sport_app/router/router.dart';
 import 'package:sport_app/theme/theme.dart';
 
+class SportApp extends StatelessWidget {
+  const SportApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: first_theme,
+      routes: router,
+    );
+  }
+}
---
+++ lib/theme/theme.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+final first_theme = ThemeData(
+        primarySwatch: Colors.blue,
+      );
